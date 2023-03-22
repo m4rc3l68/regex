7c1e359dc5112f4a12a0785a90c59260da144345
marcel <marcelinocost69@gmail.com>
2023-03-22 18:27:01 -0300

    commit - 02

--- js/regex.js
+++ js/regex.js
@@ -3,12 +3,21 @@
  * o método .exec() de RegExp.
  */
 const myRe1 = /d(b+)d/g;
-const myRe3 = new RegExp('d(b+)d', 'g');
-
 const myArray1 = myRe1.exec('cdbbdbsbz');
-const myArray2 = /d(b+)/g.exec('cdbbdbsbz');
+
+/* 
+const myArray2 = /d(b+)d/g.exec('cdbbdbsbz');
+const myRe3 = new RegExp('d(b+)d', 'g');
 const myArray3 = myRe3.exec('cdbbdbsbz');
 
 console.log(myArray1);
 console.log(myArray2);
-console.log(myArray3);
+console.log(myArray3); 
+*/
+
+console.log('O último índice é ' + myRe1.lastIndex);
+//Resp: O último índice é 5
+
+let myArray = /d(b+)d/g.exec('cdbbdbsbz');
+console.log('O último índice é ' + /d(b+)d/g.lastIndex);
+//Resp: O último índice é 0
