fdd0d804aa387dab85267bfd85cc5fc4536e3345
marcel <marcelinocost69@gmail.com>
2023-03-23 19:09:10 -0300

    commit - 03

--- js/regex.js
+++ js/regex.js
@@ -15,9 +15,59 @@ console.log(myArray2);
 console.log(myArray3); 
 */
 
-console.log('O último índice é ' + myRe1.lastIndex);
+// console.log('O último índice é ' + myRe1.lastIndex);
 //Resp: O último índice é 5
 
 let myArray = /d(b+)d/g.exec('cdbbdbsbz');
-console.log('O último índice é ' + /d(b+)d/g.lastIndex);
+// console.log('O último índice é ' + /d(b+)d/g.lastIndex);
 //Resp: O último índice é 0
+
+//Usando as Substrings entre Parênteses na ER:
+const re = /(\w+)\s(\w+)/;
+const str = 'Marcelino Costa';
+const novaStr = str.replace(re, '$2, $1');
+// console.log(novaStr);
+
+// const re4 = /\w+\s/g;
+const re4 = new RegExp('\\w+\\s', 'g');
+const str4 = 'fee fi fo fum';
+const myArray4 = str4.match(re4);
+// console.log(myArray4);
+
+//Alterando o pedido em uma string de entrada:
+const nomes =
+  'Maria Letícia ;Angel Scott ;Helen Roche ;João Costa ;Benedita Silva';
+
+const saida = ['---------- String original\n' + nomes, '\n'];
+
+let padrao = /\s+;\s+/;
+
+const nomeLista = nomes.split(padrao);
+
+padrao = /(\w+)\s+(\w+)/;
+
+const bySurnameList = []; //porLista de Sobrenomes
+
+saida.push('---------- Após Separar pela Expressão Regular');
+
+let i, len;
+for (i = 0, len = nomeLista.length; i < len; i++) {
+  saida.push(nomeLista[i]);
+  bySurnameList[i] = nomeLista[i].replace(padrao, '$2, $1');
+}
+
+//Exibe a nova matriz.
+saida.push('---------- Nomes Invertidos');
+for (i = 0, len = bySurnameList.length; i < len; i++) {
+  saida.push(bySurnameList[i]);
+}
+
+// Classifica pelo sobrenome e exibe a matriz classificada.
+bySurnameList.sort();
+saida.push('---------- Ordenado');
+for (i = 0, len = bySurnameList.length; i < len; i++) {
+  saida.push(bySurnameList[i]);
+}
+
+saida.push('---------- Fim');
+console.log(saida.join('\n'));
